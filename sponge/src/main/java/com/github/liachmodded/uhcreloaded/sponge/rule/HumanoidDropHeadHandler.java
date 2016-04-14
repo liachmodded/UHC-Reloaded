f1b664bb8dceb3531a4ae3954e3cf25cf6fbdfcd
liach <liach@users.noreply.github.com>
2016-04-14 16:55:04 -0700

    Dummy

---
+++ sponge/src/main/java/com/github/liachmodded/uhcreloaded/sponge/rule/HumanoidDropHeadHandler.java
@@ -0,0 +1,86 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) liachmodded <https://github.com/liachmodded>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.liachmodded.uhcreloaded.sponge.rule;
+
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.type.SkullTypes;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntitySnapshot;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.entity.Item;
+import org.spongepowered.api.entity.living.Humanoid;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.DestructEntityEvent;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.item.inventory.ItemStackSnapshot;
+
+import java.util.Optional;
+
+/**
+ * Created by liach on 4/12/2016.
+ *
+ * @author liach
+ */
+public final class HumanoidDropHeadHandler {
+
+    public static final HumanoidDropHeadHandler INSTANCE = new HumanoidDropHeadHandler();
+
+    private HumanoidDropHeadHandler() {
+    }
+
+    @Listener
+    public void onEvent(DestructEntityEvent.Death event) {
+        if (!(event.getTargetEntity() instanceof Player)) {
+            return;
+        }
+        Player player = (Player) event.getTargetEntity();
+        ItemStack itemStack = ItemStack.builder()
+                .itemType(ItemTypes.SKULL)
+                .quantity(1)
+                .add(Keys.SKULL_TYPE, SkullTypes.PLAYER)
+                .add(Keys.REPRESENTED_PLAYER, player.getProfile())
+                .build();
+        ItemStackSnapshot itemStackSnapshot = itemStack.createSnapshot();
+        EntitySnapshot snapshot = EntitySnapshot.builder()
+                .type(EntityTypes.ITEM)
+                .position(player.getTransform().getPosition())
+                .world(player.getWorld().getProperties())
+                .build();
+        Optional<Entity> optEntity = snapshot.restore();
+        if (!optEntity.isPresent()) {
+            throw new RuntimeException();
+        }
+        Entity entity = optEntity.get();
+        if (!(entity instanceof Item)) {
+            throw new RuntimeException();
+        }
+        Item item = (Item) entity;
+        item.offer(Keys.REPRESENTED_ITEM, itemStackSnapshot);
+    }
+
+}
